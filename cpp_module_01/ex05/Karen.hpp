863d6cc6a652fba903dd0c0177438fc6fca6d0e4
Sungbeom Shin <sushin@c10r2s2.42seoul.kr>
2021-10-13 21:32:18 +0900

    working cpp_module_01 ex05

---
+++ cpp_module_01/ex05/Karen.hpp
@@ -0,0 +1,41 @@
+#ifndef KAREN_HPP
+# define KAREN_HPP
+
+
+#include <iostream>
+
+class Karen
+{
+private:
+	/* data */
+public:
+	Karen(/* args */);
+	~Karen();
+};
+
+Karen::Karen(/* args */)
+{
+}
+
+Karen::~Karen()
+{
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#endif
