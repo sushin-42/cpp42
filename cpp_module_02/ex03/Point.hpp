dd70a0fe3d8ce03124eb2de118caa572c04caf00
Sungbeom Shin <sushin@c8r2s11.42seoul.kr>
2021-10-14 21:58:56 +0900

    working_2 cpp_module_02 ex03

--- cpp_module_02/ex03/Point.hpp
+++ cpp_module_02/ex03/Point.hpp
@@ -3,5 +3,33 @@
 
 #include "Fixed.hpp"
 
+class Point
+{
+private:
+	Fixed const _x;
+	Fixed const _y;
+
+public:
+	Point(void);
+	Point(Point const &srcObject);
+	Point(Point const &srcObject);// working
+	~Point(void);
+
+	Fixed	getX(void)
+	{
+		return (_x);
+	};
+	Fixed	getY(void)
+	{
+		return (_y);
+	};
+};
+
+Point::Point(void) : _x(0), _y(0){}
+Point::Point(Point const &srcObject) : _x(srcObject._x), _y(srcObject._y){}
+
+Point::~Point()
+{
+}
 
 #endif
